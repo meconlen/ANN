4fd8efce66a5f3d6d76a3a8d85116a4ea976fef2
Michael Conlen <mike@conlen.org>
2019-05-12 12:03:42 -0400

    setup input weights

--- ANN.py
+++ ANN.py
@@ -6,3 +6,5 @@ class basic_ANN:
 		self.input_nodes = input_n
 		self.hidden_nodes = hidden_n
 		self.output_nodes = output_n
+		self.input_weights = np.random.randn(self.hidden_nodes, self.input_nodes);
+		
--- test.py
+++ test.py
@@ -6,15 +6,15 @@ import ANN
 
 class Test_basic_ANN(unittest.TestCase):
 	def test_init(self):
-		self.network = ANN.basic_ANN(2, 2, 2)
+		self.network = ANN.basic_ANN(2, 3, 2)
 		self.assertTrue(self.network.input_nodes == 2)
-		self.assertTrue(self.network.hidden_nodes == 2)
+		self.assertTrue(self.network.hidden_nodes == 3)
 		self.assertTrue(self.network.output_nodes == 2)
-
+		print self.network.input_weights
 
 def main():
 	print("Testing")
-	np.__config__.show()
+#	np.__config__.show()
 	unittest.main()
 
 if __name__=="__main__":
