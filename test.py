effe15c3764982c7d746314c1452740d654ef9d0
Michael Conlen <mike@conlen.org>
2019-05-12 11:46:29 -0400

    setup unit testing framework

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
 import numpy as np
+import unittest
+import ANN
+
+class Test_basic_ANN(unittest.TestCase):
+	def test_init(self):
+		self.network = ANN.basic_ANN(2, 2, 2)
+
 
 def main():
 	print("Testing")
 	np.__config__.show()
-
+	unittest.main()
 
 if __name__=="__main__":
 	main()
